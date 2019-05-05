64648864cdfac6c4ae65a6aa21b912b9ab812d11
Nicola Menonna <nicomenonna@gmail.com>
2019-05-05 10:43:08 +0200

    models, views e templates

---
+++ v_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/nico/wa/python/corso_django3/progetto_portfolio_django/v_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
